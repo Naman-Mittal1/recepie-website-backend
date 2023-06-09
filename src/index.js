9c557297f83c28fb99efb78c78869541a019ec63
Naman Mittal <71561855+Naman-Mittal1@users.noreply.github.com>
2023-06-09 19:59:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import mongoose from "mongoose";
 import { userRouter } from "./routes/user.js";
 import { recipesRouter } from "./routes/recipes.js";
+import dotenv from "dotenv";
+
+dotenv.config(); 
 
 const app = express();
 
@@ -12,12 +15,9 @@ app.use(cors());
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
 
-mongoose.connect(
-  MONGODB_URL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(3001, () => console.log("Server started"));
